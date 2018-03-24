baaf0e7125da6a94b3362ef79803abd11907ef9e
MChen9 <37673773+MChen9@users.noreply.github.com>
2018-03-23 20:57:40 -0500

    Update 500 jumps.m

--- Nonhomogeneous Poisson Process/500 jumps.m
+++ Nonhomogeneous Poisson Process/500 jumps.m
@@ -4,5 +4,5 @@ e = exprnd(1,1,500); % tau_1,tau_2.. are IID Exp
 for i = 1: 500
     t(i) = sum(e(1:i));
 end 
-m = (t.*3).^(1/3); % inverse 
+m = (t.*3).^(1/3); % inverse for lambda(t)=t^2
 stairs([0,m], 0:length(m))
