020c3ed3cf8712835b226d345fb12b8369494ea5
MChen9 <37673773+MChen9@users.noreply.github.com>
2018-03-23 20:59:50 -0500

    Update time less than15.m

--- Nonhomogeneous Poisson Process/time less than15.m
+++ Nonhomogeneous Poisson Process/time less than15.m
@@ -3,11 +3,11 @@ i=0;
 t=0;
 vec = [];
 m = 0;
-    while m <15;
-        i = i+1;
-        t = t+exp(1);
-        m = (t*3)^(1/3); % inverse for lambda(t)=t^2
-        vec = [vec,m];
-    end
+while m <15;
+     i = i+1;
+     t = t+exp(1);
+     m = (t*3)^(1/3); % inverse for lambda(t)=t^2
+     vec = [vec,m];
+end
     
 stairs([0,vec], 0:length(vec))
