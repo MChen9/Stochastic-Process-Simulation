42af522c9b14749f15e5a5e8606bd0b77798fe9f
MChen9 <37673773+MChen9@users.noreply.github.com>
2018-03-23 20:55:30 -0500

    Update 500 jumps.m

--- Nonhomogeneous Poisson Process/500 jumps.m
+++ Nonhomogeneous Poisson Process/500 jumps.m
@@ -1,8 +1,8 @@
 %% simulates this process until 500 jumps
 clear all 
-e = exprnd(1,1,500);
+e = exprnd(1,1,500); % tau_1,tau_2.. are IID Exp
 for i = 1: 500
     t(i) = sum(e(1:i));
 end 
-m = (t.*3).^(1/3);
+m = (t.*3).^(1/3); % inverse 
 stairs([0,m], 0:length(m))
